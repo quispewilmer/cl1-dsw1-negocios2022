0d21100cdd0d267570398a5fe3a2934579ea098a
quispewilmer <wilmerquispecode@gmail.com>
2022-09-26 21:24:35 -0500

    Nah, switching to dotnet core

--- cl1-q1/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cl1_q1.MvcApplication" Language="C#" %>
---
+++ cl1-q1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cl1_q1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
